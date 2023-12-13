99fa108bdf615a4cc30101a02e6de0fbe0282646
sakhaline <alexandra.ivva@gmail.com>
2023-12-13 06:19:03 +0200

    Update ACCESS_TOKEN_LIFETIME & REFRESH_TOKEN_LIFETIME

--- planetarium_config/settings.py
+++ planetarium_config/settings.py
@@ -117,8 +117,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=2),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=5),
     "ROTATE_REFRESH_TOKENS": False,
 }
 
